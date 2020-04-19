e37a7c540d61c52de2cdf49584d24c324e046568
Ola <olawunmi263@yahoo.com>
2020-04-19 04:24:30 +0100

    Register to sell car feature added and steps generated

---
+++ src/test/java/co/uk/motors/features/RegisterOrSignInToSellACar.feature
@@ -0,0 +1,23 @@
+Feature: Register or sign in to sell a car
+  As a user
+  I want the ability to register
+  So I can sell my car
+
+  Background:
+    Given a user is on Motors homepage
+
+  Scenario Outline:
+  User is directed to register when they want to sell a car
+
+    When a user clicks on Sell
+    And user enters valid "<Email>"
+    And user clicks on the signUp button
+    Then the sign in page is displayed
+
+  Examples:
+  |Email|
+  |titi@mailinator.com|
+
+
+
+
---
+++ src/test/java/co/uk/motors/stepDefinitions/RegisterToSellACarSteps.java
@@ -0,0 +1,31 @@
+package co.uk.motors.stepDefinitions;
+
+import co.uk.motors.pages.BasePage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegisterToSellACarSteps extends BasePage {
+
+
+    @And("user enters valid {string}")
+    public void userEntersValid(String arg0) {
+
+    }
+
+    @When("a user clicks on Sell")
+    public void aUserClicksOnSell() {
+
+    }
+
+    @And("user clicks on the signUp button")
+    public void userClicksOnTheSignUpButton() {
+
+    }
+
+    @Then("the sign in page is displayed")
+    public void theSignInPageIsDisplayed() {
+
+    }
+}
+
