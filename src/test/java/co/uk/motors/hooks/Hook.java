6afb8e435488c31a2961789b06cbdb237a1bfe60
Ola <olawunmi263@yahoo.com>
2020-04-30 02:32:24 +0100

    Hook updated
    Signed-off-by: Ola <olawunmi263@yahoo.com>

--- src/test/java/co/uk/motors/hooks/Hook.java
+++ src/test/java/co/uk/motors/hooks/Hook.java
@@ -3,16 +3,32 @@ package co.uk.motors.hooks;
 import co.uk.motors.commons.Browsers;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hook extends Browsers {
     public String chromeBrowser = "Chrome";
-
+private Scenario scenario;
     @Before
-    public void setUp(){
+    public void setUp(Scenario scenario){
+        this.scenario = scenario;
         launchBrowser(chromeBrowser);
     }
     @After
     public void tearDown(){
+        if (scenario.isFailed())
+
+        {
+            try{
+                byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+                scenario.embed(screenshot,"screenshot.png","failurePage");
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
         closeBrowser();
     }
 }
